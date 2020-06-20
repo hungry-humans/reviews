7887de7153d3c6268b7839f8b9715b8258a09814
Nathan Sou <nathanksou@gmail.com>
2020-06-19 20:30:50 -0700

    Serve static file from server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const db = require('../database/index.js');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('./public'));
 
 app.get('/biz/:business_id/reviews', (req, res) => {
   const queryString = `SELECT * FROM reviews INNER JOIN users
