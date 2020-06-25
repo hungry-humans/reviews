677c62348667e61e7b31bda72a1126a807129bae
Nathan Sou <nathanksou@gmail.com>
2020-06-24 20:52:07 -0700

    Implement skeleton of example and write section of the reviews header

---
+++ client/components/ExampleBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExampleBox = (props) => (
+  <div className="example-box">
+    <img src="https://7elp-photos.s3-us-west-1.amazonaws.com/empty_user_profile.png"></img>
+  </div>
+);
+
+export default ExampleBox;
